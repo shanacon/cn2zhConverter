628f3aae0072464f1b85cf252e9cc450610aa94a
shanacon <weston123741852@gmail.com>
2022-04-10 21:35:56 +0800

    add execption when using gb2312 encode

--- ReadEvent.py
+++ ReadEvent.py
@@ -1,6 +1,11 @@
+from tkinter import E
 import chardet
 
 def ReadData(path):
     text = open(path, 'rb').read()
-    LoadF = open(path, "r" , encoding = chardet.detect(text)["encoding"])
-    return LoadF.readlines()
+    if chardet.detect(text)["encoding"] == 'GB2312':
+        LoadF = open(path, "r" , encoding = 'gb18030')
+    else :
+        LoadF = open(path, "r" , encoding = chardet.detect(text)["encoding"])
+    data =  LoadF.readlines()
+    return data
